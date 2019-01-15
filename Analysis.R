b6d89fb8a4f682336033e9260a448c7ad045451b
kamorr20 <42624035+kamorr20@users.noreply.github.com>
2019-01-15 11:52:08 -0700

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,3 +1,14 @@
 #Pseudocode description of what we are trying to do
-# Where the data is 
-file_path="/mnt/storage/DATASETS/INVESTOR_DATA/MELISSA/dbgap_8834/10091050/files"
+
+# Access the GTEx data
+agave.asu.edu
+newgrp wilsonsayreslab
+/mnt/storage/DATASETS/INVESTOR_DATA/MELISSA/dbgap_8834/10091050/files 
+#Download read counts for GTEx
+phe000020.v1.GTEx_RNAseq.expression-data-matrixfmt.c1/ 
+#Download ASE counts for GTEx
+phe000024.v1.GTEx_ASE_SNPs.expression-matrixfmt-ase.c1/
+#Messing around with data
+cat GTEx_Data_20160115_v7_RNAseq_RNASeQCv1.1.8_metrics.tsv | awk '{print $1,$2,$3,$4}' | less
+
+
